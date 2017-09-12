cb28bf5b02a4961babe2d8a9817585b11ce5d776
fredy180 <fredyjosevergaracardenas@gmail.com>
2017-09-11 22:19:21 -0500

    23
    23

--- php/sesion.php
+++ php/sesion.php
@@ -1,78 +1,76 @@
 <?php
-			session_start();
-$_SESSION[ "idAdmin" ]=0;	
-				if ( $_POST ) {
-					
-					require( "conexion.php" );
-					
-					$id = $_POST[ 'usuario' ];
-					$contra = $_POST[ 'password' ];
-					$tipo=$_POST['tipo'];
-					
-					
-					if($tipo=="Admin"){
-						$sql2 = $mysqli->query( "SELECT * FROM `administrador` WHERE `idadministrador`='$id'" );
-					if ( $fila = mysqli_fetch_assoc( $sql2 ) ) {
+session_start();
+$_SESSION[ "idAdmin" ] = 0;
+if ( $_POST ) {
 
-						if ( $id == $fila[ 'idadministrador' ] ) {
-							if ( $contra == $fila[ 'nombreAdministrador' ] ) {
-								$_SESSION["idAdmin"] =$_POST[ 'usuario' ];
-								echo "ok1";
-								exit();
-							} else {
+	require( "conexion.php" );
 
-								echo "Datos incorrecto";
-								exit();
-							}
-							
+	$id = $_POST[ 'usuario' ];
+	$contra = $_POST[ 'password' ];
+	$tipo = $_POST[ 'tipo' ];
 
-						}
 
-					}
-					}
-					else if($tipo=="Docen"){
-						$sql2 = $mysqli->query( "SELECT * FROM `docente` WHERE `iddocente`='$id'" );
-					if ( $fila = mysqli_fetch_assoc( $sql2 ) ) {
+	if ( $tipo == "Admin" ) {
+		$sql2 = $mysqli->query( "SELECT * FROM `administrador` WHERE `idadministrador`='$id'" );
+		if ( $fila = mysqli_fetch_assoc( $sql2 ) ) {
 
-						if ( $id == $fila[ 'iddocente' ] ) {
-							if ( $contra == $fila[ 'contraDocente' ] ) {
-								$_SESSION["idAdmin"] =$_POST[ 'usuario' ];
-								echo "ok2";
-								exit();
-							} else {
+			if ( $id == $fila[ 'idadministrador' ] ) {
+				if ( $contra == $fila[ 'nombreAdministrador' ] ) {
+					$_SESSION[ "idAdmin" ] = $_POST[ 'usuario' ];
+					echo "ok1";
+					exit();
+				} else {
 
-								echo "Datos incorrecto";
-								exit();
-							}
-							
-
-						}
-
-					}
-					}
-					else{
-						$sql2 = $mysqli->query( "SELECT * FROM `estudiante` WHERE `idestudiante`='$id'" );
-					if ( $fila = mysqli_fetch_assoc( $sql2 ) ) {
-
-						if ( $id == $fila[ 'idestudiante' ] ) {
-							if ( $contra == $fila[ 'contrasena' ] ) {
-								$_SESSION["idAdmin"] =$_POST[ 'usuario' ];
-								$_SESSION["grado"]= $fila["grado"];
-								echo "ok3";
-								exit();
-							} else {
-
-								echo "Datos incorrecto";
-								exit();
-							}
-							
-
-						}
-
-					}
-					}
-					
-					
-
+					echo "Datos incorrecto";
+					exit();
 				}
-				?>
+
+
+			}
+
+		}
+	} else if ( $tipo == "Docen" ) {
+		$sql2 = $mysqli->query( "SELECT * FROM `docente` WHERE `iddocente`='$id'" );
+		if ( $fila = mysqli_fetch_assoc( $sql2 ) ) {
+
+			if ( $id == $fila[ 'iddocente' ] ) {
+				if ( $contra == $fila[ 'contraDocente' ] ) {
+					$_SESSION[ "idAdmin" ] = $_POST[ 'usuario' ];
+					echo "ok2";
+					exit();
+				} else {
+
+					echo "Datos incorrecto";
+					exit();
+				}
+
+
+			}
+
+		}
+	} else {
+		$sql2 = $mysqli->query( "SELECT * FROM `estudiante` WHERE `idestudiante`='$id'" );
+		if ( $fila = mysqli_fetch_assoc( $sql2 ) ) {
+
+			if ( $id == $fila[ 'idestudiante' ] ) {
+				if ( $contra == $fila[ 'contrasena' ] ) {
+					$_SESSION[ "idAdmin" ] = $_POST[ 'usuario' ];
+					$_SESSION[ "grado" ] = $fila[ "grado" ];
+					echo "ok3";
+					exit();
+				} else {
+
+					echo "Datos incorrecto";
+					exit();
+				}
+
+
+			}
+
+		}
+	}
+
+
+
+}
+?>
