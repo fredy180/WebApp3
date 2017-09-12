4e1b4180b4345078a1074aae3d79c3e5f9ae2a42
fredy180 <fredyjosevergaracardenas@gmail.com>
2017-09-11 21:57:33 -0500

    5
    56

--- php/conexion.php
+++ php/conexion.php
@@ -1,6 +1,11 @@
+
 <?php
 	
-	$mysqli = new mysqli("sql309.eshost.com.ar", "eshos_19978840", "31352188", "eshos_19978840_portalmate");
+	$mysqli = new mysqli("127.0.0.1", "root", "root", "portalmate");
 if ($mysqli->connect_errno) {
     echo "Falló la conexión con MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
+
+
+
+?>
